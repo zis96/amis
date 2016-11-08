c3173a41efaa72899877b4c28853fa457f6f8dbd
Eugenij Kizim <jackjunior98@gmail.com>
2016-11-08 18:27:19 +0200

    Struct
    Обрана тема - "Радіо КПІ".

--- students_works/II_course/km52/Kizim_Yevhenii/C project/input_playlist.c
+++ students_works/II_course/km52/Kizim_Yevhenii/C project/input_playlist.c
@@ -4,32 +4,47 @@
 #include <stdlib.h>
 #include "input_playlist.h"
 #include "playlist_validation.h"
+#include <stdbool.h>
 
 P_INFO* input_playlist() {
     P_INFO* user_playlist=malloc(sizeof(P_INFO));
     char* temp_input=NULL;
     int name_length=0;
+    bool input_finish=false;
     user_playlist->path=malloc(sizeof(char)*(sizeof(".playlist")+1));
     //start input from keyboard
-    while (1) {
-        *temp_input=getchar();
-        if (*temp_input==13) {
+    while (input_finish==false) {
+        *temp_input=getch();
+        switch (*temp_input) {
+        case 27:
             if (name_length==0)
-                printf("Please enter at least one character!\n");
+                printf("\a");
             else
-                break;
-        }
-        else {
-        name_length++;
-        user_playlist->path=realloc(user_playlist->path,sizeof(char)*(sizeof(".playlsit"+name_length+1)));
-        strcat(user_playlist->path,temp_input);
+                input_finish=true;
+            break;
+        case '"':
+        case '|':
+        case '\\':
+        case '/':
+        case '*':
+        case ':':
+        case '?':
+        case '>':
+        case '<':
+            printf("\a");
+            break;
+        default:
+            name_length++;
+            user_playlist->path=realloc(user_playlist->path,sizeof(char)*(sizeof(".playlist"+name_length+1)));
+            strcat(user_playlist->path,temp_input);
+            printf("%s\r", user_playlist->path);
         }
     };
     strcat(user_playlist->path,".playlist");
     //validating input
     user_playlist->songs_counter = validate_playlist(user_playlist->path);
     if (user_playlist->songs_counter==0) {
-        printf("Playlist is empty or does not exist! \n");
+        printf("\nPlaylist is empty or does not exist! \n");
         return NULL;
     }
     else
